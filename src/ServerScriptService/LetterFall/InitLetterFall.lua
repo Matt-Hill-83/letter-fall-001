6a863ac175221e35d19d5c901ff64a802e8ca26f
Matt Hill <matt.hill.email@gmail.com>
2020-12-15 07:16:11 -0800

    asfd

--- src/ServerScriptService/LetterFall/InitLetterFall.lua
+++ src/ServerScriptService/LetterFall/InitLetterFall.lua
@@ -6,7 +6,6 @@ local ConfigGame = require(Sss.Source.LetterFall.ConfigGame)
 local LetterFall = require(Sss.Source.LetterFall.LetterFall)
 local TargetWord = require(Sss.Source.LetterFall.TargetWord)
 local HandleBrick2 = require(Sss.Source.LetterFall.HandleBrick3)
--- local HandleBrick2 = require(SP.Source.StarterPlayerScripts.HandleBrick2)
 
 HandleBrick2.initClickHandler()
 
